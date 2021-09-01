959367da58e0e3a492f8d2c43d61d2ee9c072a0e
geocodinglife <geocodinglife@gmail.com>
2021-08-31 19:50:29 -0500

    Display Grid > > Co-authored-by: juanequex <ramon.g.uz37@gmail.com> Co-authored-by: adrianMRJ <adrian.mrj.97@gmail.com>

---
+++ game-of-life/game.rb
@@ -0,0 +1,31 @@
+require 'amazing_print'
+class Game
+  attr_accessor :grid
+  def initialize(arr_size = 8)
+    @grid = Array.new(arr_size) { Array.new(arr_size) {'.'}}
+  end
+
+  def add_cell
+    7.times do
+      first_number = rand(0..7)
+      second_number = rand(0..7)
+      grid[first_number][second_number] = '*'
+    end
+  end
+
+  def display_grid
+    8.times do |number|
+      puts " #{grid[number][0]} | #{grid[number][1]} | #{grid[number][2]} | #{grid[number][3]} | #{grid[number][4]} | #{grid[number][5]} | #{grid[number][6]} | #{grid[number][7]}"
+      puts "--------------------------------" 
+    end
+  end
+
+  def show_input
+    add_cell
+    display_grid
+  end
+end
+
+
+Game.new(8).show_input
+
